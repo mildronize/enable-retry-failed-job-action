bdf529e8577c1cb70998607e895a6d93a5e28d5c
Thada Wangthammang <thada.wth@gmail.com>
2021-12-29 22:46:27 +0700

    add basic restore

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export enum Inputs {
+    Key = "key"
+}
+
+export enum Outputs {
+    LastRunStatus = "last-run-status"
+}
---
+++ src/restore.ts
@@ -0,0 +1,42 @@
+import * as core from "@actions/core";
+import * as cache from "@actions/cache";
+import * as github from "@actions/github";
+import fs from "fs";
+
+// const fs = require('fs').promises;
+
+import { Inputs, Outputs } from "./constants";
+// import * as utils from "./utils/actionUtils";
+
+const OutputName = "last_run_status";
+const LastStatusPath = "last_run_status";
+
+async function run(): Promise<void> {
+    try {
+        const uniqueKey = core.getInput(Inputs.Key) || "default_key";
+        const dateNow = (new Date()).toISOString();
+        await fs.promises.writeFile(LastStatusPath , `::set-output name=${OutputName}::default`);
+        const paths = [ LastStatusPath ];
+        const primaryKey = `${github.context.runId}-${uniqueKey}`;
+        const restoreKeys = [];
+        const cacheKey = await cache.restoreCache(
+            paths, 
+            primaryKey,
+            restoreKeys);
+        // core.saveState
+        const LastRunStatus = await fs.promises.readFile(LastStatusPath);
+        console.log(`LastRunStatus = ${LastRunStatus}`);
+        console.log(`cacheKey = ${cacheKey}`);
+        // if (primaryKey === cacheKey){
+        //     core.setOutput(Outputs.LastRunStatus, );
+        // }
+        core.setOutput(Outputs.LastRunStatus, "HEY output");
+  
+    } catch (error) {
+        core.setFailed(`Action failed with error ${error}`);
+    }
+}
+
+run();
+
+export default run;
